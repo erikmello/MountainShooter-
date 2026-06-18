d9050777ba6e46c90a32b31bdd7f8d9b8b803f83
Erik <contato.erikmello@hotmailcom>
2026-06-18 14:59:03 -0300

    Connecting Menu and Game Classes

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class Menu:
-    def __init__(self):
-        self.window = None
 
-    def run(self, ):
+class Menu:
+    def __init__(self, window):
+        self.window = window
+
+    def run(
+        self,
+    ):
         pass
